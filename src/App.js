0735b31db7347e975e95d3ebf55aa11dc1e713fa
jibanranjan <jdebata427@gmail.com>
2023-09-03 22:07:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MovieSearch from './components/MovieSerach';
+
+function App() {
+  return (
+    <div className="App">
+     <MovieSearch />
+    </div>
+  );
+}
+
+export default App;
